4b483fb22c92d08e88df404106a2eebb02366c74
talaini mohammed <talainimohammed@gmail.com>
2023-03-16 05:19:55 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int intType;
+        float floatType;
+        char charType;
+	long int longType;
+	long long int longlongType;
+        
+	printf("Size of char: %zu byte(s)\n", sizeof(charType));
+        printf("Size of int: %zu byte(s)\n", sizeof(intType));
+        printf("Size of long int: %zu byte(s)\n", sizeof(longType));
+        printf("Size of long long int: %zu byte(s)\n", sizeof(longlongType));
+        printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+	return (0);
+}
+
+
+

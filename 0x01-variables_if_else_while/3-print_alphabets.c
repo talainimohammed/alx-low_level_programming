b4eae9b88a1b6863932a32f77965efb5b82cd9e5
talaini mohammed <talainimohammed@gmail.com>
2023-03-17 08:49:08 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'A'; a <= 'Z'; a++)
+        {
+                putchar(a);
+        }
+	putchar('\n');
+	return (0);
+}

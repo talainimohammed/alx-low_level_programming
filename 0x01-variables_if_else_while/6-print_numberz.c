2a7ca85c11119ccfa2be1ab7e3d14716001cede8
talaini mohammed <talainimohammed@gmail.com>
2023-03-17 09:14:35 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar('0' + a);
+	}
+	putchar('\n');
+	return (0);
+}

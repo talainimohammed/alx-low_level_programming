26a9ce0a0b466144eca2d31347783cc32822aa41
talaini mohammed <talainimohammed@gmail.com>
2023-03-17 07:50:13 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,10 @@
 #include <time.h>
 #include<stdio.h>
 /**
- *betty style doc for function main goes there
- **/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;

f56226bac2d7106608c4a95d2709e8075f49719f
talaini mohammed <talainimohammed@gmail.com>
2023-03-17 08:40:06 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a[]="abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for(i=0; i < 26; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}

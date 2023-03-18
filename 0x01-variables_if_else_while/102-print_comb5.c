595595b34da69853c805241fafb358fd21f7ec34
Talaini <mohatala@users.noreply.github.com>
2023-03-18 11:55:36 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,47 @@
-#include <stdlib.h>
-#include <time.h>
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints numbers between 00 to 99.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+int i, e, g, h, op1, op2;
 
-int i, j, d1, d2, d3, d4;
-
-for (i = 0; i < 100; i++) 
+i = e = g = h = 48;
+while (h < 58)
 {
-	for (j = i; j < 100; j++) 
+	g = 48;
+	while (g < 58)
 	{
-		d1 = i / 10;
-		d2 = i % 10;
-		d3 = j / 10;
-		d4 = j % 10;
-		putchar(d1 + '0');
-		putchar(d2 + '0');
-		putchar(' ');
-		putchar(d3 + '0');
-		putchar(d4 + '0');
-		if (i != 99 || j != 99) 
+		e = 48;
+		while (e < 58)
 		{
-			putchar(',');
-			putchar(' ');
+			i = 48;
+			while (i < 58)
+			{
+				op1 = (h * 10) + g;
+				op2 = (e * 10) + i;
+				if (op1 < op2)
+				{
+					putchar(h);
+					putchar(g);
+					putchar(' ');
+					putchar(e);
+					putchar(i);
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+				i++;
+			}
+			e++;
 		}
+		g++;
 	}
+	h++;
 }
 putchar('\n');
 return (0);

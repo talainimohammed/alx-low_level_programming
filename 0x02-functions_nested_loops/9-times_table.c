ab24ff23d334000158ec65c04f85627059eeaecf
talaini mohammed <talainimohammed@gmail.com>
2023-03-21 16:31:10 -0700

    task 9-11

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,16 +15,22 @@ void times_table(void)
 			if (k <= 9)
 			{
 				_putchar('0' + k);
-				_putchar (',');
-				_putchar (' ');
-				_putchar (' ');
+				if (j != 9)
+				{
+					_putchar (',');
+					_putchar (' ');
+					_putchar (' ');
+				}
 			}
 			else
 			{
 				_putchar('0' + (k / 10));
 				_putchar('0' + (k % 10));
-				_putchar (',');
-				_putchar (' ');
+				if (j != 9)
+				{
+					_putchar (',');
+					_putchar (' ');
+				}
 			}
 		}
 		_putchar ('\n');

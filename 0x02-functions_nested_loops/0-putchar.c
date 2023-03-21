3b34240f132d6a29071bcbd4200323717c878c93
Talaini <mohatala@users.noreply.github.com>
2023-03-21 15:14:58 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-int i = 0;
-char str[8] = "_putchar";
+        int i = 0;
+        char str[8] = "_putchar";
 
-while (i < 8)
-{
-        _putchar(str[i]);
-        i++;
-}
-_putchar('\n');     
-return (0);
+        while (i < 8)
+        {
+                _putchar(str[i]);
+                i++;
+        }
+        _putchar('\n');     
+        return (0);
 }

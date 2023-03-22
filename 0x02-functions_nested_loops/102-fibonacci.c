e603e67c04e2c2096f7572abbd7847169e8f25c8
Talaini <mohatala@users.noreply.github.com>
2023-03-22 09:56:49 +0000

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,11 +14,11 @@ int main(void)
 	
 	fib[0] = 1;
 	fib[1] = 2;
-        for(int i=2; i<n; i++)
+        for(i = 2; i < n; i++)
 	{
 		fib[i] = fib[i-1] + fib[i-2];
 	}
-	for(i=0; i<n; i++)
+	for(i = 0; i < n; i++)
 	{
 		printf("%d", fib[i]);
 		if(i != n-1)

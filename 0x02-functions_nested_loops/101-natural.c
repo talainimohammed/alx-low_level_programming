9d66edf3a02da463e6537a02a7737003c16ce22e
talaini mohammed <talainimohammed@gmail.com>
2023-03-22 02:48:50 -0700

    Task 12

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
- * print_times_table - prints the  times table.
+ * main- prints the  times table.
  *
- * @n : int value
  **/
 int main(void)
 {
 	int sum = 0;
-	
-	for (int i = 0; i < 1024; i++)
+	int i;
+
+	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{

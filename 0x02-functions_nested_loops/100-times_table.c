fd760f3eb89a30cfbf3f30661082bf8a9e9f5eee
Talaini <mohatala@users.noreply.github.com>
2023-03-22 09:24:40 +0000

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,46 +8,17 @@
  **/
 void print_times_table(int n)
 {
-	int row;
-	int column;
-	int product;
-	
-	if (n < 0 || n > 15)
+	if (n < 0 || n > 15) 
 	{
-		_putchar(' ');
-	}
-	for (row = 0; row <= n; row++)
-	{
-		for (column = 0; column <= n; column++)
-		{
-			product = (row * column);
+    		return; // do not print anything if n is out of range
+  	}
 
-			if (column == 0)
-			{
-				_putchar('0' + product);
-			}
-			else if (product <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				printf("%d", product);
-			}
-			else if (product > 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				printf("%d", product);
-			}
-			else if (product > 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-				printf("%d", product);
-			}
-		}
-		_putchar('\n');
-	}
+  	for (int i = 0; i <= n; i++) 
+	{
+    		for (int j = 0; j <= n; j++) 
+		{
+      			printf("%d ", i * j);
+    		}
+    	printf("\n");
+  	}
 }

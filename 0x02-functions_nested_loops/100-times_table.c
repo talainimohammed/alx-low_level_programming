07138736fb2322c12dd7d38f3662df847f0f4a20
Talaini <mohatala@users.noreply.github.com>
2023-03-22 09:26:03 +0000

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@ void print_times_table(int n)
 	int i, j;
 	if (n < 0 || n > 15) 
 	{
-    		return; // do not print anything if n is out of range
+    		return;
   	}
 
   	for (i = 0; i <= n; i++) 

964ad1951be21826461ef372f2a4c2e42c044f33
talaini mohammed <talainimohammed@gmail.com>
2023-03-22 02:52:34 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- prints the  times table.
+ *
+ * Return: value 0.
+ **/
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}

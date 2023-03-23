858ab755e3b7f1bad786341905e4e3e31f11c036
talaini mohammed <talainimohammed@gmail.com>
2023-03-23 08:06:18 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,15 +17,17 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < i; j++)
+			for (j = 0; j <= i; j++)
 			{
-				_putchar(' ');
 				if (i == j)
 				{
 					_putchar(92);
 					_putchar('\n');
 				}
+				else
+					_putchar(' ');
 			}
 		}
 	}
 }
+

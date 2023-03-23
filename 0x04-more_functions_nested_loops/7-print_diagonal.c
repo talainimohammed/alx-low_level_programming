dd49f248114216d7fc1bcc7b38acad0edefec129
Talaini <talainimohammed@gmail.com>
2023-03-23 12:32:27 +0000

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ *
+ * @n: lenght diagonal
+ **/
+void print_diagonal(int n)
+{
+	int i, j;
+
+    if (n == 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+	    for (i = 0; i < n; i++)
+	    {
+		    for (j = 0; j < i; j++)
+		    {
+			    _putchar(' ');
+                if (i == j)
+                {
+                    _putchar(92);
+                    _putchar('\n');
+                }
+		    }
+	    }
+    }
+}

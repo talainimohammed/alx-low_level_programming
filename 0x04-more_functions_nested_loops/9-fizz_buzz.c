86c1556022b3764186625f7b51869b70e3feab64
Talaini <talainimohammed@gmail.com>
2023-03-23 13:41:44 +0000

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - print fizz buzz numbers
+ *
+ * return: 0 value
+ **/
+int main(void)
+{
+	int i, k, l;
+
+	for (i = 1; i <= 100; i++)
+	{
+        k = i % 3;
+        l = i % 5;
+		if (k == 0 && l == 0)
+		{
+			printf("FizzBuzz ");
+		}
+        else if (k == 0)
+        {
+			printf("Fizz ");
+		}
+        else if (l == 0)
+        {
+			printf("Buzz");
+		}
+        else 
+        {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return (0);
+}

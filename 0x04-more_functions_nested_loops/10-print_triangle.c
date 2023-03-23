cab540bdb29944e3a1b78703ee234d6d60fe5ac8
Talaini <talainimohammed@gmail.com>
2023-03-23 14:07:45 +0000

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ *
+ * @size: lenght triangle
+ **/
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i > size; i--)
+		{
+			for (j = 1; j > size - i; j++)
+			{
+				_putchar(' ');
+			}
+            for (j = size - i; j <= size; j++)
+			{
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}

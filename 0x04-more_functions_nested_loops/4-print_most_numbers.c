bddf63ca18fcd037096cdad6000a13d2d3ec773e
talaini mohammed <talainimohammed@gmail.com>
2023-03-23 07:42:58 -0700

    task 4-5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,12 @@ void print_most_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if (i != 2 || i != 4)
+		if (i != 2)
 		{
-			_putchar('0' + i);
+			if (i != 4)
+			{
+				_putchar('0' + i);
+			}
 		}
 	}
 	_putchar('\n');

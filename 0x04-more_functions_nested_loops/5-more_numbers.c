1f6b54942d5968a4280f2df40f10ddccfb0b7da6
talaini mohammed <talainimohammed@gmail.com>
2023-03-23 05:10:49 -0700

    task 4-5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,10 +10,10 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-        for (j = 0; j < 10; j++)
-        {
-            _putchar('0' + j);
-        }
-        _putchar('\n');
+		for (j = 0; j < 10; j++)
+		{
+			_putchar('0' + j);
+		}
+		_putchar('\n');
 	}
 }

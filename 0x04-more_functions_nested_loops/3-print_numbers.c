96d3b97ee31f74b9b1acce31ed9cfae50cd3d0a8
talaini mohammed <talainimohammed@gmail.com>
2023-03-23 04:57:49 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  **/
 void print_numbers(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        _putchar('0' + i);
-    }
-    _putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
 }

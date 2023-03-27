d6a99849a23a568fe357d5c1892a5a4d4969865d
talaini mohammed <talainimohammed@gmail.com>
2023-03-27 15:27:49 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 - check the code 
+ * reset_to_98 - check the code
  *
+ * @n: input value.
  */
-
 void reset_to_98(int *n)
 {
-         *n = 98;
-} 
+	*n = 98;
+}

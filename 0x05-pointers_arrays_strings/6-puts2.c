ecac51d94ee007eeaee2f21a82ec5bcbc865d6e5
Talaini <talainimohammed@gmail.com>
2023-03-27 23:37:46 +0000

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,13 @@
  */
 void puts2(char *str)
 {
+	int i = 0;
+    
 	while (*str != '\0')
 	{
-		_putchar(*str);
-		str += 2;
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+		i++;
 	}
 	_putchar ('\n');
 }

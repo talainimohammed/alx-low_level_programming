2401773e9d11c7818c8b21ceea09f6de9c85a63d
Talaini <talainimohammed@gmail.com>
2023-03-27 23:34:10 +0000

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _puts2 - check the code
+ * puts2 - check the code
  *
  * @str: input value1.
  *
  */
-void _puts2(char *str)
+void puts2(char *str)
 {
 	while (*str != '\0')
 	{

2a81ac0cd609545b56b808cf245ca21edbd7dfb8
Talaini <talainimohammed@gmail.com>
2023-03-27 23:41:58 +0000

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * puts_half - prints half of a string
+  * @str: the string
+  *
+  */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(str + i) != 0)
+		i++;
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i - 1) / 2;
+	while (*(str + j) != 0)
+	{
+		_putchar(*(str + j));
+		half++;
+	}
+	_putchar('\n');
+}

79c8c6485a7c71ede96a8cee9a49bc405097d890
talaini mohammed <talainimohammed@gmail.com>
2023-03-27 15:48:16 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,12 +9,12 @@
  */
 int _strlen(char *s)
 {
-    int c = 0;
-    
-    while (*s != '\0')
-    {
-        c++;
-        *s++;
-    }
+	int c = 0;
+	
+	while (*s != '\0')
+	{
+		c++;
+		*s++;
+	}
 	return (c);
 }

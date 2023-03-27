507d42d001282100935967d3107142e0dbfb23ad
Talaini <talainimohammed@gmail.com>
2023-03-27 22:31:51 +0000

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ *
+ * @a: input value1.
+ * @b: input value2.
+ */
+void swap_int(int *a, int *b)
+{
+    int *c = *a;
+
+	*a = *b;
+    *b = *c;
+}

2a07b490805a499492c5148e9da55cef78ea584d
Talaini <talainimohammed@gmail.com>
2023-03-29 14:36:24 +0000

    Updates

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,11 +3,10 @@
 /**
  * print_number - prints an integer.
  * @n: input integer.
- * Return: no return.
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int m, d, c;
 
 	if (n < 0)
 	{
@@ -20,16 +19,16 @@ void print_number(int n)
 	}
 
 	d = m;
-	count = 1;
+	c = 1;
 
 	while (d > 9)
 	{
 		d /= 10;
-		count *= 10;
+		c *= 10;
 	}
 
-	for (; count >= 1; count /= 10)
+	for (; c >= 1; c /= 10)
 	{
-		_putchar(((m / count) % 10) + 48);
+		_putchar(((m / c) % 10) + 48);
 	}
 }

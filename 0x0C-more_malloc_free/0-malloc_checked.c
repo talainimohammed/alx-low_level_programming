a86e1693ae1eab864c13aa0a834cfae179c643e1
Talaini <talainimohammed@gmail.com>
2023-04-13 20:52:03 +0000

    Task 0-3

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: size
+ *
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}

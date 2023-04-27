f6557541c4ac09fea1b0ab6183baf5461b321128
Talaini <talainimohammed@gmail.com>
2023-04-27 15:50:14 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,7 +17,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	new->str = strdup(str);
 
-	for (nbchar = 0; str[nchar]; nbchar++)
+	for (nbchar = 0; str[nbchar]; nbchar++)
 		;
 
 	new->len = nbchar;

b95966f78072aa795a4675c97c3a1615dd7804fb
Talaini <talainimohammed@gmail.com>
2023-05-08 12:27:12 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, l);
+	w = write(f, text_content, l);
 
 	if (f == -1 || w == -1)
 		return (-1);

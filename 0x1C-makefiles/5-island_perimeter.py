c0d26078e6f2bb7ce51df3cc4606d8bfe2855c08
Talaini <talainimohammed@gmail.com>
2023-08-23 11:54:19 +0100

    updates

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""import module"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described"""
+    count = 0
+    l = len(grid) - 1
+    w = len(grid[0]) - 1
+
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    count += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    count += 1
+                if j == w or grid[i][j + 1] != 1:
+                    count += 1
+                if i == l or grid[i + 1][j] != 1:
+                    count += 1
+    return count
